35e5bd841a9817dc15907f770628fff12698518a
Kimberly Lewis <kblewis.flt@gmail.com>
2016-01-30 14:25:49 -0600

    scope fix

--- app/components/app.ts
+++ app/components/app.ts
@@ -3,7 +3,8 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'main-window',
     templateUrl: 'app/components/scene.html',
-    styleUrls: ['app/components/style.css']
+    styleUrls: ['app/components/style.css'],
+    encapsulation: ViewEncapsulation.Native
 })
 export default class Scene {
 
